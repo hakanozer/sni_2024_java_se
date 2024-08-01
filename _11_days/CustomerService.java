177d910204f755fd52f7b9ac536f5361b8028284
Hakan ÖZER <hakanozer02@gmail.com>
2024-08-01 19:30:22 +0300

    11-Days

--- _11_days/CustomerService.java
+++ _11_days/CustomerService.java
@@ -3,20 +3,23 @@ package _11_days;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import _11_days.entities.Customer;
 
 public class CustomerService {
     
-    DB db = new DB();
+    private DB db = new DB();
 
-    public int customerAdd( String name, String surname, String email, String password ) {
+    public int customerAdd( Customer customer ) {
         int status = 0;
         try {
             String sql = "insert into customer values(default,?,?,?,?)";
             PreparedStatement pre = db.conn().prepareStatement(sql);
-            pre.setString(1, name);
-            pre.setString(2, surname);
-            pre.setString(3, email);
-            pre.setString(4, password);
+            pre.setString(1, customer.getName());
+            pre.setString(2, customer.getSurname());
+            pre.setString(3, customer.getEmail());
+            pre.setString(4, customer.getPassword());
             status = pre.executeUpdate();
         } catch (Exception e) {
             System.err.println("customerAdd error:" + e);
@@ -27,7 +30,8 @@ public class CustomerService {
     }
 
 
-    public void customerAll() {
+    public ArrayList<Customer> customerAll() {
+        ArrayList<Customer> customeArrayList = new ArrayList<>();
         try {
             String sql = "select * from customer";
             PreparedStatement pre = db.conn().prepareStatement(sql);
@@ -38,13 +42,72 @@ public class CustomerService {
                 String surname = rs.getString("surname");
                 String email = rs.getString("email");
                 String password = rs.getString("password");
-                System.out.println(id + "-" + name + "-" + surname + "-" + email + "-" + password);
+                Customer customer = new Customer(id, name, surname, email, password);
+                customeArrayList.add(customer);
             }
         } catch (Exception e) {
             System.err.println("customerAll error: " + e);
         }finally {
             db.close();
         }
+        return customeArrayList;
+    }
+
+
+    public int customerDelete(long id) {
+        int status = 0;
+        try {
+            String sql = "delete from customer where id = ?";
+            PreparedStatement pre = db.conn().prepareStatement(sql);
+            pre.setLong(1, id);
+            status = pre.executeUpdate();
+        } catch (Exception e) {
+            System.err.println("customerDelete error: " + e);
+        }finally {
+            db.close();
+        }
+        return status;
+    }
+
+    public int customerUpdate(Customer customer) {
+        int status = 0;
+        try {
+            String sql = "update customer set name = ?, surname = ?, email = ?, password = ? where id = ?";
+            PreparedStatement pre = db.conn().prepareStatement(sql);
+            pre.setString(1, customer.getName());
+            pre.setString(2, customer.getSurname());
+            pre.setString(3, customer.getEmail());
+            pre.setString(4, customer.getPassword());
+            pre.setLong(5, customer.getId());
+            status = pre.executeUpdate();
+        } catch (Exception e) {
+            System.err.println("customerUpdate error: " + e);
+        }finally {
+            db.close();
+        }
+        return status;
+    }
+
+    public Customer customerLogin(String email, String password) {
+        try {
+            String sql = "select * from customer where email = ? and password = ?";
+            PreparedStatement pre = db.conn().prepareStatement(sql);
+            pre.setString(1, email);
+            pre.setString(2, password);
+            ResultSet rs = pre.executeQuery();
+            if (rs.next()) {
+                long id = rs.getLong("id");
+                String name = rs.getString("name");
+                String surname = rs.getString("surname");
+                Customer customer = new Customer(id, name, surname, email, password);
+                return customer;
+            }
+        } catch (Exception e) {
+            System.err.println("customerLogin error: " + e);
+        }finally {
+            db.close();
+        }
+        return null;
     }
 
 
--- _11_days/MainApp.java
+++ _11_days/MainApp.java
@@ -1,13 +1,36 @@
 package _11_days;
 
+import java.util.ArrayList;
+
+import _11_days.entities.Customer;
+
 public class MainApp {
     public static void main(String[] args) {
 
         CustomerService customerService = new CustomerService();
-        //int status = customerService.customerAdd("Leyla", "Bilirler", "leyla@mail.com", "12345");
-        //System.out.println(status);
 
-        customerService.customerAll();
+        //Customer addCustomer = new Customer(0, "Erkan", "Bilirler", "erkan@mail.com", "12345");
+        //int addStatus = customerService.customerAdd(addCustomer);
+        //System.out.println("addStatus: " + addStatus);
+
+        int deleteStatus = customerService.customerDelete(3);
+        System.out.println("deleteStatus: " +deleteStatus);
+
+        Customer customer = new Customer(2, "Serkan", "Bilsin", "serkan@mail.com", "12345");
+        int updateStatus = customerService.customerUpdate(customer);
+        System.out.println("updateStatus: " + updateStatus);
+
+        Customer loginCustomer = customerService.customerLogin("kaan@mail.com", "12345");
+        if (loginCustomer == null) {
+            System.out.println("Email or Password Fail");
+        }else {
+            System.out.println("Login Success: " + loginCustomer);
+        }
+
+        ArrayList<Customer> ls = customerService.customerAll();
+        for (Customer item : ls) {
+            System.out.println(item);
+        }
 
     }
 }
